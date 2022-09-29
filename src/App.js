f0327e31c9880f51e3988760785e0d467ab3f1f5
kyaching <kyochingmarma@gmail.com>
2022-09-29 10:28:10 +0600

    added questions component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Activities from "./components/Activities/Activities";
+import Questions from "./components/Questions/Questions";
 
 function App() {
   return (
     <div className="App">
       <Activities></Activities>
+      <Questions></Questions>
     </div>
   );
 }
---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div>
+      <div>
+        <h1>How does react works?</h1>
+        <p>
+          Ans: React is a Javascript library. React mainly focused on the User
+          Interface of a website.React doesn’t have many moving parts for us to
+          learn about and understand. Reactjs is simple. The advantage to having
+          a small API to work with is that you can spend more time familiarizing
+          yourself with it, experimenting with it, and so on. The syntax used by
+          React components is called JSX (JavaScript XML). The idea is actually
+          quite simple. A component renders content by returning some JSX. The
+          JSX itself is usually HTML markup, mixed with custom tags for the
+          React components. What’s absolutely groundbreaking here is that we
+          don’t have to perform little micro-operations to change the content of
+          a component.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
